c605baf06344e7eee10888dd1d8574655a0c0b7f
David Darke <david@atomicsmash.co.uk>
2013-04-21 17:39:42 +0100

    SVN commit

---
+++ js/video-embed.js
@@ -0,0 +1,97 @@
+(function($){
+
+	/*
+	*  acf/setup_fields
+	*
+	*  This event is triggered when ACF adds any new elements to the DOM. 
+	*
+	*  @type	function
+	*  @since	1.0.0
+	*  @date	01/01/12
+	*
+	*  @param	event		e: an event object. This can be ignored
+	*  @param	Element		postbox: An element which contains the new HTML
+	*
+	*  @return	N/A
+	*/
+	
+	//GetContent(document.getElementById(&quot;id_field&quot;).value,&quot;id&quot;);"
+	//GetContent(document.getElementById(&quot;search_field&quot;).value,&quot;search&quot;);
+	$(document).live('acf/setup_fields', function(e, postbox){
+
+		$(".youtubesearch").click(function(){
+			
+		GetContent("dogs","search");
+			
+		});
+
+		function GetContent(foreign_id,view_type)
+		{
+			var default_id = "itpdwd"; // Define default YouTube username ID here
+			var max_videos = 4;  // How many to show
+			
+			if (!foreign_id)
+			{	foreign_id = default_id;
+				view_type = "id";
+			}
+			
+			// Compute the request URL (either by username or by search term)
+			//  Note: "alt=json-in-script&callback=?" params are in URL to allow cross-domain JSON using jQuery
+			//  See: http://docs.jquery.com/Ajax/jQuery.getJSON#urldatacallback
+			var url;
+			if (view_type=="id")
+				url = "http://gdata.youtube.com/feeds/api/users/" + escape(foreign_id) + "/uploads?max-results=" + max_videos +
+					"&alt=json-in-script&callback=?";
+			else
+				url = "http://gdata.youtube.com/feeds/api/videos?vq=" + escape(foreign_id) + "&max-results=" + max_videos +
+					"&alt=json-in-script&callback=?";
+			
+			// Clear the HTML display
+			document.getElementById("videos").innerHTML = "";
+			
+			// Get the data from the web service and process
+			$.getJSON(url,
+				function(data)
+				{
+					$.each(data.feed.entry, function(i,item)
+					{
+						// Extract video ID from the videos API ID
+						var api_id = item.id.$t; api_id.match(/\/(\w+?)$/);
+						var id = RegExp.$1;
+						
+						var title = item.title.$t;
+						
+						// Get first 10 chars of date_taken, which is the date: YYYY-MM-DD
+						var date_pub = item.published.$t.substring(0, 10);
+						
+						// Collect the authors
+						var author_text = "";
+						$.each(item.author, function(j,item2)
+						{	if (author_text) author_text += ", ";
+							author_text += '<a href="' + item2.uri.$t + '">' + item2.name.$t + '</a>';
+						});
+						
+						// Format the HTML for this video
+						var text = '<div align="left" class="video">' +
+							'<b class="title">' + title + '</b><br/>' +
+							'Published: ' + date_pub + ' by ' + author_text + '<br/>' +
+							'<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/' + id + '&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><embed src="http://www.youtube.com/v/' + id + '&hl=en&fs=1" type="application/x-shockwave-flash" allowfullscreen="true" width="425" height="344"></embed></object></div>';
+						
+						// Now append to the HTML display
+						$(text).appendTo("#videos");
+			
+					});
+				}
+			);
+		
+		}
+
+
+	});
+
+			
+
+
+})(jQuery);
+
+
---
+++ video-embed-v4.php
@@ -0,0 +1,418 @@
+<?php
+
+class acf_video_embed extends acf_field
+{
+	// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+		
+		
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function __construct()
+	{
+		// vars
+		$this->name = 'video';
+		$this->label = __('Video');
+		$this->category = __("Content",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field. 
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'preview_size' => 'thumbnail'
+		);
+		
+		
+		// do not delete!
+    	parent::__construct();
+    	
+    	
+    	// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+	
+	
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+	
+	function create_options( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		
+		$field['character_number'] = isset($field['character_number']) ? $field['character_number'] : '';
+		
+		?>
+		<tr class="field_option field_option_<?php echo $this->name; ?>">
+			<td class="label">
+				<label><?php _e("Number of characters",'acf'); ?></label>
+			</td>
+			<td>
+				<?php 
+				
+				
+				if($field['character_number'] == ""){
+					
+					$field['character_number'] = 150;
+					
+				}
+			
+				do_action('acf/create_field', array(
+							'type'	=>	'text',
+							'name'	=>	'fields['.$field['name'].'][character_number]',
+							'value'	=>	$field['character_number']
+				));
+		
+
+				
+				
+				?>
+			</td>
+		</tr>
+
+<!--
+		<tr class="field_option field_option_file">
+			<td class="label">
+				<label>Choice</label>
+			</td>
+			<td>
+				<ul class="radio_list radio horizontal"><li><label><input id="acf-field_5_save_format-object" type="radio" name="fields[field_5][save_format]" value="object">File Object</label></li><li><label><input id="acf-field_5_save_format-url" type="radio" name="fields[field_5][save_format]" value="url">File URL</label></li><li><label><input id="acf-field_5_save_format-id" type="radio" name="fields[field_5][save_format]" value="id" checked="checked" data-checked="checked">File ID</label></li></ul>			</td>
+		</tr>	
+-->	
+		
+		<?php
+		
+		
+
+		
+
+		
+	}
+	
+	
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function create_field( $field )
+	{
+		
+		//useable code
+/*
+		$field['value'] = str_replace('<br />','',$field['value']);
+		echo '<textarea id="' . $field['id'] . '" class="limiterField" data-characterlimit="'.$field['character_number'].'" rows="4" class="' . $field['class'] . '" name="' . $field['name'] . '" >' . $field['value'] . '</textarea>';
+		
+		echo('<div id="progressbar-'.$field['id'].'" class="progressBar"></div>');
+*/
+		
+		?>
+		
+		
+		
+<h3>YouTube Data API Example: Using JSON and JQuery to Search</h3>
+YouTube ID: <input type="text" size="30" id="id_field" value="itpdwd">
+<input type="button" value="Get by ID" onclick="GetContent(document.getElementById(&quot;id_field&quot;).value,&quot;id&quot;);"> &nbsp; &nbsp; 
+Search Term: <input type="text" size="30" id="search_field" value="soccer">
+<input type="button" value="Search" class="youtubesearch" onclick=""> &nbsp; &nbsp; 
+<input type="button" value="Hide" onclick="document.getElementById(&quot;videos&quot;).innerHTML = &quot;&quot;;">
+<p>
+</p><div id="videos"></div>
+<p></p>
+
+
+
+		
+		
+		
+		<?
+		
+		
+		
+		
+		
+		
+				
+		
+	}
+	
+	
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add css + javascript to assist your create_field() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_enqueue_scripts()
+	{
+		
+	wp_enqueue_script('jquery');
+		// register acf scripts
+		wp_register_script( 'acf-limiter', $this->settings['dir'] . 'js/video-embed.js', array('jquery'), $this->settings['version'] );
+		
+		
+		//jquery-ui-progressbar
+		
+		
+		// scripts
+		wp_enqueue_script(array(
+			'acf-limiter',	
+		));
+
+
+		
+	}
+	
+	
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add css and javascript to assist your create_field() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_head()
+	{
+		// Note: This function can be removed if not used
+
+		
+		
+	}
+	
+	
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add css + javascript to assist your create_field_options() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+	}
+
+	
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add css and javascript to assist your create_field_options() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  load_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value found in the database
+	*  @param	$post_id - the $post_id from which the value was loaded from
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the value to be saved in te database
+	*/
+	
+	function load_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  update_value()
+	*
+	*  This filter is appied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+	
+	function update_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  format_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed to the create_field action
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+	
+	function format_value( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['preview_size'] to alter the $value?
+		
+		
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+	
+	function format_value_for_api( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['preview_size'] to alter the $value?
+		
+		
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  load_field()
+	*
+	*  This filter is appied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$field - the field array holding all the field options
+	*/
+	
+	function load_field( $field )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+	
+	
+	/*
+	*  update_field()
+	*
+	*  This filter is appied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field( $field, $post_id )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+
+	
+}
+
+
+// create field
+new acf_video_embed();
+
+?>
